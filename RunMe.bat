9265abf8a6ac87adad36026944533668a7c4a749
Gigoo <robster105@yahoo.it>
2020-09-30 03:59:50 -0400

    -Fix bad commits

--- RunMe.bat
+++ RunMe.bat
@@ -13,21 +13,10 @@ REM Update files
 call Tools\Functions\Update_function
 
 REM Install prerequisites
-<<<<<<< HEAD
 pip install -r %root%\Tools\Requirements.txt
-=======
-pip install -r %Output%\Tools\Requirements.txt
->>>>>>> f184b098eed66b918e9cee3b431c4a5248cd4ec9
 
 REM Clear screen
 CLS
 
 REM Run script
-<<<<<<< HEAD
 python %script_location%
-=======
-python %script_location%
-
-REM Pause at the end for testing purposes
-pause
->>>>>>> f184b098eed66b918e9cee3b431c4a5248cd4ec9
